6321bc0cfbe68c05d1061b45fd496032ccd632f5
Taras Polisnichenko <taras_polisnichenko@epam.com>
2022-03-24 15:38:06 +0200

    Added tests for Web project

---
+++ GameStore.Web.Tests/Controllers/GameControllerTests.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using GameStore.Core.Exceptions;
+using GameStore.Core.Interfaces;
+using GameStore.Core.Models.Games;
+using GameStore.Core.Models.Records;
+using GameStore.Web.Controllers;
+using GameStore.Web.Models;
+using GameStore.Web.Profiles;
+using GameStore.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+namespace GameStore.Web.Tests.Controllers;
+
+public class GameControllerTests
+{
+    private readonly GamesController _gameController;
+    private readonly Mock<IGameService> _gameServiceMock;
+    private readonly Mock<ICommentService> _commentServiceMock;
+
+    public GameControllerTests()
+    {
+        _gameServiceMock = GetGameServiceMock();
+        _commentServiceMock = GetCommentServiceMock();
+
+        _gameController = new GamesController(
+            _gameServiceMock.Object, _commentServiceMock.Object,
+            new Mapper(new MapperConfiguration(expression => expression.AddProfile(new WebCommonProfile()))));
+    }
+
+    [Fact]
+    public async void GetAllAsync_NoParameters_ReturnsCorrectValue()
+    {
+        var expectedResultCount = AllGamesViewModels.Count;
+
+        var actualResult = await _gameController.GetAllAsync();
+        var actualResultCount = actualResult.Count;
+        
+        Assert.Equal(expectedResultCount, actualResultCount);
+    }
+    
+    [Fact]
+    public async void GetWithDetailsAsync_ExistingKey_ReturnsGame()
+    {
+        var expectedModel = AllGamesViewModels[0];
+
+        var actualResult = await _gameController.GetWithDetailsAsync(AllGames[0].Key);
+        var actualObjectResult = actualResult.Result as OkObjectResult;
+        var actualResultModel = actualObjectResult?.Value as GameViewModel;
+
+        Assert.IsType<OkObjectResult>(actualResult.Result);
+        Assert.IsType<GameViewModel>(actualResultModel);
+        Assert.Equal(expectedModel.Id, actualResultModel.Id);
+        Assert.Equal(expectedModel.Name, actualResultModel.Name);
+    }
+    
+    [Fact]
+    public async void GetWithDetailsAsync_NotExistingKey_ThrowsNotFoundException()
+    {
+        var operation = async () => await _gameController.GetWithDetailsAsync("AllGames[0].Key");
+
+        await Assert.ThrowsAsync<ItemNotFoundException>(operation);
+    }
+    
+    [Fact]
+    public async void GetFileAsync_ExistingKey_ReturnsGame()
+    {
+        var expectedResult = AllGames[0].File;
+
+        var actualResult = await _gameController.GetFileAsync(AllGames[0].Key);
+        
+        Assert.Equal(expectedResult, (actualResult.Result as OkObjectResult)?.Value);
+    }
+    
+    [Fact]
+    public async void GetFileAsync_NotExistingKey_ThrowsNotFoundException()
+    {
+        var operation = async () => await _gameController.GetWithDetailsAsync("AllGames[0].Key");
+
+        await Assert.ThrowsAsync<ItemNotFoundException>(operation);
+    }
+
+    [Fact]
+    public async void CreateAsync_CorrectValue_ReturnsModel()
+    {
+        var expectedGameKey = AllGames[1].Key;
+        var createModel = new GameCreateRequestModel
+        {
+            Name = AllGames[1].Name,
+            Description = AllGames[1].Description,
+            File = AllGames[1].File
+        };
+
+        var actualResult = await _gameController.CreateAsync(createModel);
+        var actualObjectResult = actualResult.Result as OkObjectResult;
+        var actualResultModel = actualObjectResult?.Value as GameViewModel;
+
+        Assert.IsType<OkObjectResult>(actualResult.Result);
+        Assert.IsType<GameViewModel>(actualResultModel);
+        Assert.Equal(expectedGameKey, actualResultModel.Key);
+    }
+    
+    [Fact]
+    public async void CommentGameAsync_CorrectValue()
+    {
+        var createModel = new CommentCreateRequestModel
+        {
+            GameKey = AllGames[2].Key,
+            AuthorName = "Some author",
+            Message = "Some text"
+        };
+
+        var actualResult = await _gameController.CommentGameAsync(createModel);
+
+        Assert.IsType<OkResult>(actualResult);
+    }
+
+    [Fact]
+    public async void UpdateAsync_CorrectValues()
+    {
+        var editModel = new GameEditRequestModel
+        {
+            Id = AllGames[1].Id,
+            Name = AllGames[1].Name,
+            Description = AllGames[1].Description,
+            File = AllGames[1].File,
+            GenresIds = AllGames[1].Genres.Select(gg => gg.GenreId).ToList(),
+            PlatformsIds = AllGames[1].Platforms.Select(gp => gp.PlatformId).ToList()
+        };
+
+        var actualResult = await _gameController.UpdateAsync(editModel);
+
+        Assert.IsType<OkResult>(actualResult);
+    }
+    
+    [Fact]
+    public async void DeleteAsync_CorrectValues()
+    {
+        var idToDelete = AllGames[3].Id;
+        
+        var actualResult = await _gameController.DeleteAsync(idToDelete);
+
+        Assert.IsType<OkResult>(actualResult);
+    }
+    
+    private Mock<IGameService> GetGameServiceMock()
+    {
+        var mock = new Mock<IGameService>();
+
+        mock.Setup(service => service.GetAllAsync())
+            .ReturnsAsync(AllGames);
+
+        mock.Setup(service => service.GetByKeyAsync(AllGames[0].Key))
+            .ReturnsAsync(AllGames[0]);
+
+        mock.Setup(service => service.GetFileAsync(AllGames[0].Key))
+            .ReturnsAsync(AllGames[0].File);
+
+        mock.Setup(service => service.GetByKeyAsync("AllGames[0].Key"))
+            .ThrowsAsync(new ItemNotFoundException());
+
+        mock.Setup(service => service.GetFileAsync("AllGames[0].Key"))
+            .ThrowsAsync(new ItemNotFoundException());
+
+        mock.Setup(service => service.CreateAsync(
+                       It.Is<GameCreateModel>(model => model.Name == AllGames[1].Name)))
+            .ReturnsAsync(AllGames[1]);
+
+        return mock;
+    }
+    
+    private Mock<ICommentService> GetCommentServiceMock()
+    {
+        var mock = new Mock<ICommentService>();
+
+        return mock;
+    }
+    
+    private static readonly List<Game> AllGames = new()
+    {
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b971-08da067d7601"),
+            Name = "First game",
+            Key = "first-game",
+            Description = "First description",
+            File = new byte[] { 0, 0, 0, 1 },
+            IsDeleted = false
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b972-08da067d7601"),
+            Name = "Second game",
+            Key = "second-game",
+            Description = "Second description",
+            File = new byte[] { 0, 0, 0, 2 },
+            IsDeleted = false
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b973-08da067d7601"),
+            Name = "Third game",
+            Key = "Third-game",
+            Description = "Third description",
+            File = new byte[] { 0, 0, 0, 3 },
+            IsDeleted = false
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b974-08da067d7601"),
+            Name = "Fourth game",
+            Key = "fourth-game",
+            Description = "Fourth description",
+            File = new byte[] { 0, 0, 0, 4 },
+            IsDeleted = false
+        }
+    };
+    
+    private static readonly List<GameViewModel> AllGamesViewModels = new()
+    {
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b971-08da067d7601"),
+            Name = "First game",
+            Key = "first-game",
+            Description = "First description",
+            Genres = new List<GenreViewModel>(),
+            Platforms = new List<PlatformTypeViewModel>()
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b972-08da067d7601"),
+            Name = "Second game",
+            Key = "second-game",
+            Description = "Second description",
+            Genres = new List<GenreViewModel>(),
+            Platforms = new List<PlatformTypeViewModel>()
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b973-08da067d7601"),
+            Name = "Third game",
+            Key = "Third-game",
+            Description = "Third description",
+            Genres = new List<GenreViewModel>(),
+            Platforms = new List<PlatformTypeViewModel>()
+        },
+        new()
+        {
+            Id = Guid.Parse("6fd6d158-7ffd-472a-b974-08da067d7601"),
+            Name = "Fourth game",
+            Key = "fourth-game",
+            Description = "Fourth description",
+            Genres = new List<GenreViewModel>(),
+            Platforms = new List<PlatformTypeViewModel>()
+        }
+    };
+}
---
+++ GameStore.Web.Tests/Middlewares/ExceptionMiddlewareTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+using GameStore.Core.Exceptions;
+using GameStore.Web.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace GameStore.Web.Tests.Middlewares;
+
+public class ExceptionMiddlewareTests
+{
+    private readonly Mock<RequestDelegate> _requestDelegateMock;
+    private readonly Mock<HttpContext> _httpContextMock;
+    private readonly Mock<ILogger<ExceptionMiddleware>> _loggerMock;
+
+    public ExceptionMiddlewareTests()
+    {
+        _loggerMock = new Mock<ILogger<ExceptionMiddleware>>();
+        _httpContextMock = new Mock<HttpContext>();
+        _httpContextMock.Setup(context => context.Response.StatusCode)
+                        .Verifiable();
+        _requestDelegateMock = new Mock<RequestDelegate>();
+    }
+
+    [Fact]
+    public void Invoke_ThrowsItemNotFound_AddsCorrectStatusCode()
+    {
+        _requestDelegateMock.Setup(rd => rd.Invoke(It.IsAny<HttpContext>()))
+                            .ThrowsAsync(new ItemNotFoundException());
+
+        var exceptionMiddleware = new ExceptionMiddleware(_requestDelegateMock.Object, _loggerMock.Object);
+        exceptionMiddleware.Invoke(_httpContextMock.Object);
+        
+        _httpContextMock.VerifySet(context => context.Response.StatusCode = (int)HttpStatusCode.NotFound);
+    }
+    
+    [Fact]
+    public void Invoke_ThrowsInvalidOperationException_AddsCorrectStatusCode()
+    {
+        _requestDelegateMock.Setup(rd => rd.Invoke(It.IsAny<HttpContext>()))
+                            .ThrowsAsync(new InvalidOperationException());
+
+        var exceptionMiddleware = new ExceptionMiddleware(_requestDelegateMock.Object, _loggerMock.Object);
+        exceptionMiddleware.Invoke(_httpContextMock.Object);
+        
+        _httpContextMock.VerifySet(context => context.Response.StatusCode = (int)HttpStatusCode.BadGateway);
+    } 
+    
+    [Fact]
+    public void Invoke_ThrowsArgumentException_AddsCorrectStatusCode()
+    {
+        _requestDelegateMock.Setup(rd => rd.Invoke(It.IsAny<HttpContext>()))
+                            .ThrowsAsync(new ArgumentException());
+
+        var exceptionMiddleware = new ExceptionMiddleware(_requestDelegateMock.Object, _loggerMock.Object);
+        exceptionMiddleware.Invoke(_httpContextMock.Object);
+        
+        _httpContextMock.VerifySet(context => context.Response.StatusCode = (int)HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public void Invoke_ThrowsUnsupportedException_AddsCorrectStatusCode()
+    {
+        _requestDelegateMock.Setup(rd => rd.Invoke(It.IsAny<HttpContext>()))
+                            .ThrowsAsync(new Exception());
+
+        var exceptionMiddleware = new ExceptionMiddleware(_requestDelegateMock.Object, _loggerMock.Object);
+        exceptionMiddleware.Invoke(_httpContextMock.Object);
+        
+        _httpContextMock.VerifySet(context => context.Response.StatusCode = (int)HttpStatusCode.InternalServerError);
+    }
+}
