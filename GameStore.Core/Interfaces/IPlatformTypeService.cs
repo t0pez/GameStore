6bd4959f1a6471d5a8167c1477a06eeda92367eb
Taras Polisnichenko <taras_polisnichenko@epam.com>
2022-04-23 14:36:56 +0300

    Added interface for separate service for PlatformType

---
+++ GameStore.Core/Interfaces/IPlatformTypeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameStore.Core.Models.Games;
+using GameStore.Core.Models.ServiceModels.PlatformTypes;
+
+namespace GameStore.Core.Interfaces;
+
+public interface IPlatformTypeService
+{
+    public Task<ICollection<PlatformType>> GetAllAsync();
+    public Task<PlatformType> GetByIdAsync(Guid id);
+    public Task CreateAsync(PlatformTypeCreateModel createModel);
+    public Task UpdateAsync(PlatformTypeUpdateModel updateModel);
+    public Task DeleteAsync(Guid id);
+}
---
+++ GameStore.Core/Models/ServiceModels/PlatformTypes/PlatformTypeUpdateModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GameStore.Core.Models.ServiceModels.PlatformTypes;
+
+public class PlatformTypeUpdateModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
