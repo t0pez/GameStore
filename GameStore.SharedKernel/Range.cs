004d6c8039ff04def73557207b78f5955809a7b2
Taras Polisnichenko <taras_polisnichenko@epam.com>
2022-05-21 02:22:45 +0300

    Refactored Range

--- GameStore.SharedKernel/Range.cs
+++ GameStore.SharedKernel/Range.cs
@@ -1,34 +1,93 @@
-﻿using System;
+﻿#nullable enable
+using System;
+using System.Collections;
 
 namespace GameStore.SharedKernel;
 
-public readonly struct Range<T> where T : IComparable<T>
+public class Range<T> where T : struct, IComparable
 {
-    public Range(T min, T max)
+    private readonly IComparer? _comparer;
+
+    public Range(T? min, T? max, IComparer? comparer = null)
     {
-        if (min.CompareTo(max) == 1)
+        if (min is not null && max is not null && min.Value.IsGreaterThan(max))
         {
             throw new ArgumentException("Min can not be greater than max");
         }
 
         Min = min;
         Max = max;
+        _comparer = comparer;
     }
 
-    public T Min { get; }
-    public T Max { get; }
+    public T? Min { get; }
+    public T? Max { get; }
+
+    private bool IsMinEnabled => Min is not null;
+    private bool IsMaxEnabled => Max is not null;
+    private bool HasCustomComparer => _comparer is not null;
 
     public bool Contains(T element)
     {
-        return (element.CompareTo(Min) == 1 || element.CompareTo(Min) == 0) &&
-               (element.CompareTo(Max) == -1 || element.CompareTo(Max) == 0);
+        return ElementGreaterOrEqualToMin(element) &&
+               ElementLessOrEqualToMax(element);
+    }
+
+    private bool ElementLessOrEqualToMax(T element)
+    {
+        if (HasCustomComparer)
+        {
+            return element.IsLessThan(Max, _comparer) || element.IsEqualTo(Max, _comparer);
+        }
+
+        return IsMaxEnabled == false || element.IsLessThan(Max) || element.IsEqualTo(Max);
+    }
+
+    private bool ElementGreaterOrEqualToMin(T element)
+    {
+        if (HasCustomComparer)
+        {
+            return element.IsGreaterThan(Min, _comparer) || element.IsEqualTo(Min, _comparer);
+        }
+
+        return IsMinEnabled == false || element.IsGreaterThan(Min) || element.IsEqualTo(Min);
     }
 }
 
 public static class ComparableExtensions
 {
-    public static bool InRange<T>(this T element, Range<T> range) where T : IComparable<T>
+    public static bool IsInRange<T>(this T element, Range<T> range) where T : struct, IComparable
     {
         return range.Contains(element);
+    }
+
+    public static bool IsGreaterThan<T>(this T element, T? other) where T : struct, IComparable
+    {
+        return element.CompareTo(other) == 1;
+    }
+
+    public static bool IsGreaterThan<T>(this T element, T? other, IComparer comparer) where T : struct, IComparable
+    {
+        return comparer.Compare(element, other) == 1;
+    }
+
+    public static bool IsLessThan<T>(this T element, T? other) where T : struct, IComparable
+    {
+        return element.CompareTo(other) == -1;
+    }
+
+    public static bool IsLessThan<T>(this T element, T? other, IComparer comparer) where T : struct, IComparable
+    {
+        return comparer.Compare(element, other) == -1;
+    }
+
+    public static bool IsEqualTo<T>(this T element, T? other) where T : struct, IComparable
+    {
+        return element.CompareTo(other) == 0;
+    }
+
+    public static bool IsEqualTo<T>(this T element, T? other, IComparer comparer) where T : struct, IComparable
+    {
+        return comparer.Compare(element, other) == 0;
     }
 }
