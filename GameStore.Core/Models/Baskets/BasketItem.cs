84a64426a823f349401fa785a2d5de3aa12f9abd
Taras Polisnichenko <taras_polisnichenko@epam.com>
2022-07-22 14:46:36 +0300

    Removed unused services and models

--- GameStore.Core/Models/Baskets/BasketItem.cs
+++
@@ -1,9 +0,0 @@
-﻿using GameStore.Core.Models.Games;
-
-namespace GameStore.Core.Models.Baskets;
-
-public class BasketItem
-{
-    public Game Game { get; set; }
-    public int Quantity { get; set; }
-}
